99238691f1911d55b091c170e0daaa25e8b08fb8
Zengyonghao <zengyonghao@outlook.com>
2018-04-17 18:07:10 +0800

    更改整个项目的包名

--- app/src/main/java/zplh_android_yk/zplh/com/yk_20/callback/ThreadCallback.java
+++
@@ -1,7 +0,0 @@
-package zplh_android_yk.zplh.com.yk_20.callback;
-
-/**
- * Created by yong hao zeng on 2018/4/14/014.
- */
-public interface ThreadCallback {
-}
--- app/src/main/java/zplh_android_yk/zplh/com/yk_20/utils/UiautomatorUtils.java
+++
@@ -1,7 +0,0 @@
-package zplh_android_yk.zplh.com.yk_20.utils;
-
-/**
- * Created by yong hao zeng on 2018/4/14/014.
- */
-public class UiautomatorUtils {
-}
